65c8bb147dfdbd01e54821b5e3f17bfa9f4ce5bf
Adithya <adithyaas2217@gmail.com>
2026-09-11 22:45:13 +0530

    Initial portfolio frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 import About from "./component/About";
 import Skills from "./component/Skill";
-import Projects from "./component/project";
+import Projects from "./component/Project";
 import Contact from "./component/Contact";
 import Footer from "./component/Footer";
 
